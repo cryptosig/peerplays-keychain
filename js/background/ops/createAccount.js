f0bf6d6dc2c61f0d5810dd76712b3265af36a6e4
cryptosig <7476547+cryptosig@users.noreply.github.com>
2021-05-15 06:45:37 -0700

    inital pass of operations, needs heavy modification and testing

---
+++ js/background/ops/createAccount.js
@@ -0,0 +1,26 @@
+const broadcastCreateClaimedAccount = data => {
+  return new Promise((resolve, reject) => {
+    peerplays.broadcast.createClaimedAccount(
+      key,
+      data.username,
+      data.new_account,
+      JSON.parse(data.owner),
+      JSON.parse(data.active),
+      data.memo,
+      {},
+      [],
+      (err, result) => {
+        console.log(result, err);
+        const err_message = beautifyErrorMessage(err);
+        const message = createMessage(
+          err,
+          result,
+          data,
+          chrome.i18n.getMessage("bgd_ops_create_account", [data.new_account]),
+          err_message
+        );
+        resolve(message);
+      }
+    );
+  });
+};
